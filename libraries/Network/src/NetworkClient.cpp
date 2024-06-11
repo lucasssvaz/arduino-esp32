4714f3bc43f8e4c0c11ed88372d27ac4a571fade
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-06-11 11:52:04 -0300

    test

--- libraries/Network/src/NetworkClient.cpp
+++ libraries/Network/src/NetworkClient.cpp
@@ -23,6 +23,7 @@
 #include <lwip/netdb.h>
 #include <errno.h>
 
+
 #define IN6_IS_ADDR_V4MAPPED(a) ((((__const uint32_t *)(a))[0] == 0) && (((__const uint32_t *)(a))[1] == 0) && (((__const uint32_t *)(a))[2] == htonl(0xffff)))
 
 #define WIFI_CLIENT_DEF_CONN_TIMEOUT_MS (3000)
