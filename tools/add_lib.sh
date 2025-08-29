751980e3f342c17872bebad4669be8d0e3144fe3
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2025-08-29 17:33:28 -0300

    Unfixable commit

--- tools/add_lib.sh
+++ tools/add_lib.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 HELP="This script help to add library when using arduino-esp32 as an ESP-IDF component
 The script accepts up to three arguments:
 -n  NEW: URL address to new library on GIThub (cannot be combined with -e)
